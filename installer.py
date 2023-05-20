3dce3eac0899f086c7ca53799ec83f0d464d9c52
Daniil Ermolaev <blcklptn@icloud.com>
2023-05-21 03:50:19 +0500

    save

---
+++ installer.py
@@ -0,0 +1,16 @@
+import json
+import os
+import sys
+
+# Check system platform Linux/OSX
+def platform_checker():
+    pass
+
+# Install docker from binary
+def docker_installer():
+    pass
+
+# config_reader():
+class ConfigReader:
+    pass
+
