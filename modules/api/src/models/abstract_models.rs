36fb8aeb837e4639738eb8af12d807fbb3564015
Daniil Ermolaev <blcklptn@icloud.com>
2023-05-22 01:05:36 +0500

    Abstract Data Models ✅
    Abstract models for other structs

---
+++ modules/api/src/models/abstract_models.rs
@@ -0,0 +1,32 @@
+use serde::{ Deserialize, Serialize };
+
+
+#[allow(non_snake_case)]
+#[derive(Deserialize, Serialize, Debug, Clone)]
+pub struct AbstractUserData {
+    pub first_name: String,
+    pub middle_name: Option<String>,
+    pub last_name: String,
+}
+
+#[allow(non_snake_case)]
+#[derive(Deserialize, Serialize, Debug, Clone)]
+pub struct AbstractLessonData {
+    pub teacher_id: String,
+    pub lesson_name: String,
+}
+
+#[allow(non_snake_case)]
+#[derive(Deserialize, Serialize, Debug, Clone)]
+pub struct AbstractGroupData {
+    pub main_teacher_id: String,
+    pub group_name: String,
+    pub group_short_name: Option<String>,
+}
+
+#[allow(non_snake_case)]
+#[derive(Deserialize, Serialize, Debug, Clone)]
+pub struct AbstractTokenData {
+    pub token: String,
+    pub exp: i64,
+}
