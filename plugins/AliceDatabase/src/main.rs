56e24b2aac8e9f7065969e62070ca08d33d7a0b1
Daniil Ermolaev <blcklptn@icloud.com>
2024-09-28 09:53:10 +0500

    Config.json

--- plugins/AliceDatabase/src/main.rs
+++ plugins/AliceDatabase/src/main.rs
@@ -19,16 +19,16 @@ use remore_db::*;
 use queue_db::*;
 
 struct AliceDatabase {
-    pub log_db_mod: LogDatabase,
-    pub remore_db_mod: RemoreDBInstance,
-    pub queue_db_mod: Queue,
+    pub log_db_mod: Option<LogDatabase>,
+    pub remore_db_mod: Option<RemoreDBInstance>,
+    pub queue_db_mod: Option<Queue>,
 }
 
 impl AliceDatabase {
     pub fn new(init_all: bool) -> Self {
-        let mut log_db_mod = LogDatabase::new(PathBuf::from("./LOGDATABASE"));
-        let mut remore_db_mod = RemoreDBInstance::new("name".to_string(), Some("127.0.0.1".to_string()), Some(12_u8), "SUPERKEY".to_string());
-        let mut queue_db_mod =Queue::new();
+        let mut log_db_mod = Some(LogDatabase::new(PathBuf::from("./LOGDATABASE")));
+        let mut remore_db_mod = Some(RemoreDBInstance::new("name".to_string(), Some("127.0.0.1".to_string()), Some(12_u8), "SUPERKEY".to_string()));
+        let mut queue_db_mod = Some(Queue::new());
         AliceDatabase { log_db_mod, remore_db_mod, queue_db_mod}
     }
 }
